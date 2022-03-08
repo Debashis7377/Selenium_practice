43548b0e0ee612b6a9b0f2a1a657ae950928d6ef
Debashis <debashisbehera7377@gmail.com>
2022-03-08 10:59:48 +0530

    Selenium interview question

---
+++ Selenium_question-Imp.py
@@ -0,0 +1,21 @@
+'''
+1. Selenium Architechture
+2. latest Version 4.0
+3. Locators
+4. Difference between close() and quit()
+5. they may ask you to write x_path for a perticular scenario
+6. Dependent and independent technique of writting of x_path
+7. How do you read data from web table (td/ tr)                  # imp
+8. How do you chech if the content of the table are in sorted order or not    # imp
+9. CSS Selector  (Fastest Locator   ?)
+10.si gle slash and double slash difernece (/ and //)
+11. Counting all the links, images etc and also find the elements
+12.Unexpected tagname and StaleElementReferenceException, NoSuchElementException, TimeOutException, # most important
+13.Difference between Explicit(web driver wait) and Implicit wait
+14.Window handelling
+15.Difference between Absolute and Relative x_path
+16.Difference between find_element(return only web_element) and find_elements(List of web elements)
+17.How do you check Brocken links
+18.How do you verify the colour of web element
+19.How do you get the location and size of a perticular web element
+'''
